73f47e158cab31be240e73d028416a26f16d1d44
Suyash3385 <suyashsharma942@gmail.com>
2025-06-17 13:48:04 +0530

    Initial commit

---
+++ src/AuthForm.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+export default function AuthForm(){
+    const [isLogin,setIsLogin]=useState(true);
+    return (
+        <div className='container'>
+            <div className='form-container'>
+                <div className='form-toggle'>
+        <button className={isLogin ? 'active' : ""}onClick={()=> setIsLogin(true)}>Login</button>
+        <button className={!isLogin ? 'active' : ""}onClick={()=> setIsLogin(false)}>SignUp</button>
+                </div>
+                {isLogin ? <>
+                <div className='form'>
+                    <h2>Login Form</h2>
+
+        <input type='email' placeholder='Email'/> 
+        <input type='password' placeholder ='password'/>
+        <a href='#'>forgot password?</a> 
+        <button>Login</button>  
+        <p>Not a member? <a href='#' omClick={()=> setIsLogin(false)}>SignUp now</a></p>            
+        </div>
+                </> : <>
+                <div className='form'>
+                 <h2>SignUp Form</h2>
+
+        <input type='email' placeholder='Email'/> 
+        <input type='password' placeholder ='password'/>
+        <input type='password' placeholder ='Confirm password'/>
+        <button>SignUp</button>
+        </div>
+                </>}
+            </div>
+        </div>
+    )
+}
